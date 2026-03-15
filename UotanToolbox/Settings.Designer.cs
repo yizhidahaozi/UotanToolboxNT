3228689548df1d9849eabcc50e696328985687cd
暮间雾 <lrh15563910083@163.com>
2026-03-15 13:15:28 +0800

    Add new settings and update designer version
    Add user settings: UseNative (default changed from True to False), BootPatchPath and BackupPath to App.config/Settings.settings, and expose BootPatchPath in Settings.Designer.cs. Also bump the SettingsSingleFileGenerator GeneratedCodeAttribute version to 18.4.0.0 and adjust related default values/formatting.

--- UotanToolbox/Settings.Designer.cs
+++ UotanToolbox/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace UotanToolbox {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -49,7 +49,7 @@ namespace UotanToolbox {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool UseNative {
             get {
                 return ((bool)(this["UseNative"]));
@@ -58,7 +58,7 @@ namespace UotanToolbox {
                 this["UseNative"] = value;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
@@ -70,7 +70,7 @@ namespace UotanToolbox {
                 this["BootPatchPath"] = value;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
